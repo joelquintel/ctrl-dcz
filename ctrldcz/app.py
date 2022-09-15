5fa0c17e2e580f14e0c701d34d8d449ffc6f93bd
joelquintel <joel.quintel@hotmail.com>
2022-09-14 22:21:08 -0300

    função ola

--- ctrldcz/app.py
+++ ctrldcz/app.py
@@ -1,6 +1,20 @@
 def start():
+   
     print("CTRL-CDZ no ar")
 
+def ola():
+     text = input("digite o seu nome ")
+     robo = "r2 d2"
+     print("Olá " + text)
+     input("tudo bem? ")
+     respRobo = input("eu sou o " + robo + " posso ajudar? ")
+     ##print(respRobo)
+     if(respRobo == "pode" or respRobo =="sim"): print("que bom XD")
+     elif(respRobo == "talvez"): print("vou fazer o possivel") 
+     else: print("que pena") 
+         
 
 if __name__ == "__main__":
     start()
+    ola()
+    
